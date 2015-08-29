34416c86399ec0ed19f6434f8ecf10e61dcc9e16
Dave Teply <davehamdan@gmail.com>
2015-08-29 06:27:12 -0700

    removing 1 level of timeout, doing other clean up

--- dev-app/src/app/components/ngTextRoll/ngtextroll.js
+++ dev-app/src/app/components/ngTextRoll/ngtextroll.js
@@ -25,25 +25,30 @@
         scope.$watch('value', function(newVal, oldVal) {
           if (newVal !== oldVal) {
 
-            // 1
+            // clear timeout
+            $timeout.cancel(strAnim);
+
+            // reset position and set strings
             scope.styl1.top = h + 'px';
             scope.styl2.top = '0';
             scope.styl1.transition = '';
             scope.styl2.transition = '';
             scope.str2 = scope.str1;
+            scope.str1 = String(scope.displayValue);
 
-            $timeout(function() {
-              // 2
-              scope.str1 = String(scope.displayValue);
-              $timeout(function() {
-                // 3
-                scope.styl1.transition = t;
-                scope.styl2.transition = t;
-                scope.styl2.top = '-' + h + 'px';
-                scope.styl1.top = '0';
-              });
+            // animate
+            var strAnim = $timeout(function() {
+              scope.styl1.transition = t;
+              scope.styl2.transition = t;
+              scope.styl2.top = '-' + h + 'px';
+              scope.styl1.top = '0';
             });
           }
+        });
+
+        // clear timeout
+        scope.$on('destroy', function() {
+          $timout.cancel(strAnim);
         });
 
       };
