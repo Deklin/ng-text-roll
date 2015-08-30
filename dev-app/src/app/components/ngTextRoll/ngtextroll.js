464107a35ba2a40ef35fa381c301c663f82b4ec2
Dave Teply <davehamdan@gmail.com>
2015-08-30 08:47:40 -0700

    Changing containers around

--- dev-app/src/app/components/ngTextRoll/ngtextroll.js
+++ dev-app/src/app/components/ngTextRoll/ngtextroll.js
@@ -14,34 +14,37 @@
 
       var linkFunc = function(scope, element) {
 
-        var h = element.children()[0].offsetHeight;
         var t = 'top 0.5s ease';
         scope.styl1 = {};
         scope.styl2 = {};
 
-        // set initial binding
+        // set initial string value
         scope.str1 = String(scope.displayValue || scope.value || '');
 
         scope.$watch('value', function(newVal, oldVal) {
           if (newVal !== oldVal) {
-
+            // get height
+            var ch = element.children()[0].offsetHeight;
+            var h = element[0].offsetHeight;
             // clear timeout
             $timeout.cancel(strAnim);
 
-            // reset position and set strings
-            scope.styl1.top = h + 'px';
-            scope.styl2.top = '0';
+            // disable animation, set new string and position outside
+            //  of overflow region
             scope.styl1.transition = '';
-            scope.styl2.transition = '';
-            scope.str2 = scope.str1;
+            scope.styl1.top = h + 'px';
             scope.str1 = String(scope.displayValue);
+            // disable animation, set old string to currently viewable
+            scope.styl2.transition = '';
+            scope.styl2.top = '-' + ch + 'px';
+            scope.str2 = scope.str1;
 
-            // animate
+            // animate on next tick
             var strAnim = $timeout(function() {
               scope.styl1.transition = t;
-              scope.styl2.transition = t;
-              scope.styl2.top = '-' + h + 'px';
               scope.styl1.top = '0';
+              scope.styl2.transition = t;
+              scope.styl2.top = '-' + (ch + h + 1) + 'px';
             });
           }
         });
