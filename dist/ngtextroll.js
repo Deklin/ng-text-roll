cac9b64afd4a6227f127fb189ba6ba47f98a151f
Dave Teply <davehamdan@gmail.com>
2016-04-01 05:27:51 -0700

    characters roll independently now

--- dist/ngtextroll.js
+++ dist/ngtextroll.js
@@ -15,64 +15,84 @@
 
       // local vars
       svc.current = 0;
-      svc.render = [{
-        style: {}
-      }, {
-        style: {}
-      }];
+      svc.render = [{}, {}];
       // Constants
-      svc.trans = 'top 0.5s ease-in-out';
       svc.zero = '0';
 
       svc.init = function(height, target, currency) {
         svc.height = height;
         svc.currency = currency;
-        svc.intHeight = parseInt(svc.height);
+        svc.intHeight = parseFloat(svc.height);
         svc.unitHeight = svc.height.replace(svc.intHeight, '');
         svc.offset = svc.intHeight * 0.17;
         svc.topAbove = ((svc.intHeight + svc.offset) * -1) + svc.unitHeight;
         svc.topBelow = (svc.intHeight + svc.offset) + svc.unitHeight;
         // set initial render
-        svc.render[0].style = {
+        svc.render[svc.current].style = {
           'top': svc.zero
         };
-        svc.render[0].target = svc.formatCurrency(target);
+        svc.render[svc.current].target = svc.targetArray(target);
       };
 
-      svc.formatCurrency = function(target) {
-        return svc.currency ? $filter('currency')(target) : target;
+      svc.targetArray = function(target) {
+        return svc.currency ? $filter('currency')(target) : String(target);
       };
 
       svc.next = function() {
         svc.current = (svc.current === 0 ? 1 : 0);
       };
 
+      svc.randDec = function(min, max) {
+        return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+      };
+
+      svc.trans = function() {
+        return 'top '.concat(svc.randDec(0.3, 0.8), 's ease-in-out');
+      };
+
       svc.animSetup = function(oldVal, newVal, pos) {
-        svc.render[svc.current].style['-webkit-transition'] = undefined;
-        svc.render[svc.current].style['-moz-transition'] = undefined;
-        svc.render[svc.current].style.transition = undefined;
-        svc.render[svc.current].style.top = pos ? svc.topBelow : svc.topAbove;
-        svc.render[svc.current].target = svc.formatCurrency(newVal);
+        svc.render[svc.current].target = svc.targetArray(newVal);
+        svc.render[svc.current].style = [];
+        angular.forEach(svc.render[svc.current].target, function() {
+          svc.render[svc.current].style.push({
+            '-webkit-transition': undefined,
+            '-moz-transition': undefined,
+            'transition': undefined,
+            'top': pos ? svc.topBelow : svc.topAbove
+          });
+        });
 
         var inx = svc.current === 0 ? 1 : 0;
-        svc.render[inx].style['-webkit-transition'] = undefined;
-        svc.render[inx].style['-moz-transition'] = undefined;
-        svc.render[inx].style.transition = undefined;
-        svc.render[inx].style.top = svc.zero;
-        svc.render[inx].target = svc.formatCurrency(oldVal);
+        svc.render[inx].target = svc.targetArray(oldVal);
+        svc.render[inx].style = [];
+        angular.forEach(svc.render[inx].target, function() {
+          svc.render[inx].style.push({
+            '-webkit-transition': undefined,
+            '-moz-transition': undefined,
+            'transition': undefined,
+            'top': svc.zero
+          });
+        });
+
       };
 
       svc.animate = function(pos) {
-        svc.render[svc.current].style.top = svc.zero;
-        svc.render[svc.current].style['-webkit-transition'] = svc.trans;
-        svc.render[svc.current].style['-moz-transition'] = svc.trans;
-        svc.render[svc.current].style.transition = svc.trans;
+        angular.forEach(svc.render[svc.current].style, function(s) {
+          var trans = svc.trans();
+          s['-webkit-transition'] = trans;
+          s['-moz-transition'] = trans;
+          s.transition = trans;
+          s.top = svc.zero;
+        });
 
         var inx = svc.current === 0 ? 1 : 0;
-        svc.render[inx].style['-webkit-transition'] = svc.trans;
-        svc.render[inx].style['-moz-transition'] = svc.trans;
-        svc.render[inx].style.transition = svc.trans;
-        svc.render[inx].style.top = pos ? svc.topAbove : svc.topBelow;
+        angular.forEach(svc.render[inx].style, function(s) {
+          var trans = svc.trans();
+          s['-webkit-transition'] = trans;
+          s['-moz-transition'] = trans;
+          s.transition = trans;
+          s.top = pos ? svc.topAbove : svc.topBelow;
+        });
       };
 
       svc.runAnim = function(oldVal, newVal) {
@@ -111,7 +131,7 @@
     });
 
   // template:js
-  angular.module("ui.ngTextRoll.template", []).run(["$templateCache", function($templateCache) {$templateCache.put("template/ngtextroll.html","<div class=\"ng-text-roll\" ng-style=\"{\'font-size\' : $ctrl.height }\">\r\n  {{$ctrl.svc.render[0].target}}&ngsp;\r\n  <div class=\"ng-text-roll-render\" ng-style=\"$ctrl.svc.render[0].style\">{{$ctrl.svc.render[0].target}}</div>\r\n  <div class=\"ng-text-roll-render\" ng-style=\"$ctrl.svc.render[1].style\">{{$ctrl.svc.render[1].target}}</div>\r\n</div>\r\n");}]);
+  angular.module("ui.ngTextRoll.template", []).run(["$templateCache", function($templateCache) {$templateCache.put("template/ngtextroll.html","<div class=\"ng-text-roll\" ng-style=\"{\'font-size\' : $ctrl.height }\">\n  <div class=\"ng-text-roll-container\">\n    <div style=\"position: relative;\" ng-repeat=\"pChar in $ctrl.svc.render[$ctrl.svc.current].target track by $index\">\n      <div class=\"ng-text-roll-char\" ng-style=\"$ctrl.svc.render[0].style[$index]\">{{$ctrl.svc.render[0].target[$index]}}</div>\n      <div class=\"ng-text-roll-char\" ng-style=\"$ctrl.svc.render[1].style[$index]\">{{$ctrl.svc.render[1].target[$index]}}</div>\n      {{pChar}}\n    </div>\n  </div>\n</div>\n");}]);
   // endinject
 
 })();
