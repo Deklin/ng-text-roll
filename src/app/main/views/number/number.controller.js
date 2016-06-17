43c98069e7bd7fa52a5f26b01545aa7a2a09b851
Dave Teply <davehamdan@gmail.com>
2016-06-17 07:40:52 -0700

    adding value chaning to number demo

--- src/app/main/views/number/number.controller.js
+++ src/app/main/views/number/number.controller.js
@@ -16,6 +16,31 @@
 
     vm.theOtherValue = 12345.67;
 
+    vm.operations = [{
+      label: 'Add',
+      op: 'add'
+    }, {
+      label: 'Substract',
+      op: 'sub'
+    }, {
+      label: 'Multiply',
+      op: 'mul'
+    }];
+
+    vm.changeValue = function() {
+      switch (vm.chosenOperation) {
+        case 'add':
+          vm.theOtherValue = vm.theOtherValue + vm.delta;
+          break;
+        case 'sub':
+          vm.theOtherValue = vm.theOtherValue - vm.delta;
+          break;
+        case 'mul':
+          vm.theOtherValue = vm.theOtherValue * vm.delta;
+          break;
+      }
+    };
+
   }
 
 })();
