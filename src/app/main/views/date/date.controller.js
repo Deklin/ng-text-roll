2fe893ea4848652f93f05d940d534feffd98eb43
Dave Teply <davehamdan@gmail.com>
2016-06-22 06:34:24 -0700

    minor ui updates/fixes

--- src/app/main/views/date/date.controller.js
+++ src/app/main/views/date/date.controller.js
@@ -1,48 +1,62 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-      .module('demoApp')
-      .controller('DateDemoController', DateDemoController);
+  angular
+    .module('demoApp')
+    .controller('DateDemoController', DateDemoController);
 
-    /** @ngInject */
-    function DateDemoController() {
-      var vm = this;
+  /** @ngInject */
+  function DateDemoController(/*$scope, $timeout*/) {
+    var vm = this;
 
+    vm.theValue = new Date();
+
+    vm.operations = [{
+      label: 'Add',
+      op: 'add'
+    }, {
+      label: 'Substract',
+      op: 'sub'
+    }];
+    vm.chosenOperation = vm.operations[0].op;
+
+    vm.changeValue = function() {
+      var val = new Date(vm.theValue.valueOf());
+      switch (vm.chosenOperation) {
+        case 'add':
+          val.setDate(vm.theValue.getDate() + vm.delta);
+          break;
+        case 'sub':
+          val.setDate(vm.theValue.getDate() - vm.delta);
+          break;
+      }
+      vm.theValue = val;
+    };
+
+    vm.resetValue = function() {
       vm.theValue = new Date();
+    };
 
-      vm.operations = [{
-        label: 'Add',
-        op: 'add'
-      }, {
-        label: 'Substract',
-        op: 'sub'
-      }];
-      vm.chosenOperation = vm.operations[0].op;
+    vm.rollConfig = {
+      filter: 'date',
+      filterParam1: 'MM/dd/yyyy',
+      rollBetween: true
+    };
 
-      vm.changeValue = function() {
-        var val = new Date(vm.theValue.valueOf());
-        switch (vm.chosenOperation) {
-          case 'add':
-            val.setDate(vm.theValue.getDate() + vm.delta);
-            break;
-          case 'sub':
-            val.setDate(vm.theValue.getDate() - vm.delta);
-            break;
-        }
-        vm.theValue = val;
-      };
+    // vm.triggerChange = function() {
+    //   var preValue = vm.theValue;
+    //   vm.theValue = 9999;
+    //   vm.changeTimer = $timeout(function() {
+    //     vm.theValue = preValue;
+    //   }, 250);
+    // };
+    //
+    // $scope.$on('$destroy', function() {
+    //   if (vm.changeTimer) {
+    //     $timeout.cancel(vm.changeTimer);
+    //   }
+    // });
 
-      vm.resetValue = function() {
-        vm.theValue = new Date();
-      };
-
-      vm.rollConfig = {
-        filter: 'date',
-        filterParam1: 'MM/dd/yyyy',
-        rollBetween: true
-      };
-
-    }
+  }
 
 })();
