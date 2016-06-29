cad6a59ae75629c37bc1138cce2f28920098857a
Dave Teply <davehamdan@gmail.com>
2016-06-29 06:39:23 -0700

    adding more content for currency demo

--- src/app/main/views/currency/views/case/case.controller.js
+++ src/app/main/views/currency/views/case/case.controller.js
@@ -6,10 +6,48 @@
     .controller('CasesController', CasesController);
 
   /** @ngInject */
-  function CasesController() {
+  function CasesController(currencySvc) {
     var vm = this;
 
-    vm.foo = 123;
+    vm.svc = currencySvc;
+
+    vm.cases = [{
+      "description": "Tincidunt amet praesent amet in est massa amet Nulla Nulla molestie molestie phasellus ipsum placerat dui dui Sed id sodales vel vestibulum sed Neque Nulla lectus metus",
+      "price": 22,
+      "title": "6JRRS5WBDM19 Computer Case"
+    }, {
+      "description": "varius velit elit luctus ac interdum erat eu orci gravida luctus Integer vitae gravida leo tincidunt hac velit hendrerit vestibulum porttitor amet vestibulum lectus eros Neque metus tortor placerat lectus integer",
+      "price": 70,
+      "title": "E9TIS3L48H5FI Computer Case"
+    }, {
+      "description": "erat porttitor consectetuer lectus litora quis ante torquent est velit non justo id ac perspiciatis Neque non vestibulum vitae id sit interdum sunt est eu ac metus metus",
+      "price": 92,
+      "title": "PJ05PTQH1BIM Computer Case - Refurbished"
+    }, {
+      "description": "luctus erat sed quis et amet integer justo wisi massa metus wisi sed molestie conubia arcu arcu id",
+      "price": 41,
+      "title": "XXKPGOXEM95Y Computer Case"
+    }, {
+      "description": "vestibulum phasellus gravida vestibulum amet torquent Odio vitae sem eu dui sed Integer sem",
+      "price": 20,
+      "title": "S1FX1UB57VM Computer Case"
+    }, {
+      "description": "Nulla amet vestibulum erat elit leo non dui arcu vitae est id erat pede voluptatem non sit ipsum vestibulum",
+      "price": 39,
+      "title": "2DWB0ZZ03KAI23 Computer Case"
+    }, {
+      "description": "pretium laoreet Integer ipsum ipsum pretium integer torquent interdum suspendisse sed praesent",
+      "price": 19,
+      "title": "NTYUB5U Computer Case"
+    }, {
+      "description": "integer porttitor Sed ante placerat neque suspendisse litora litora gravida integer adipiscing phasellus ridiculus ligula velit eu lacus sodales vestibulum neque vel leo Lorem",
+      "price": 19,
+      "title": "FBN3B1ACD Computer Case"
+    }, {
+      "description": "vestibulum wisi sem vitae eros volutpat velit consequat eu turpis et justo pellentesque massa elit erat Sed amet vestibulum turpis justo litora id arcu faucibus est arcu eu dolor",
+      "price": 57,
+      "title": "IOAOHRLK76NE Computer Case - Refurbished"
+    }];
 
   }
 
--- src/app/main/views/currency/views/cpu/cpu.controller.js
+++ src/app/main/views/currency/views/cpu/cpu.controller.js
@@ -12,33 +12,33 @@
     vm.svc = currencySvc;
 
     vm.cpus = [{
-      title: '1.8Ghz CPU',
-      price: 123.89,
-      desc: ''
+      "description": "amet velit eros Integer Integer molestie id Neque ac torquent perspiciatis varius consequat velit id",
+      "price": 0.8,
+      "title": "8.2GHz CPU - Refurbished"
     }, {
-      title: '2.1Ghz CPU',
-      price: 109.32,
-      desc: ''
+      "description": "neque elit hendrerit eu quis id quis id lacus nunc praesent dictumst erat pretium consequat eu lacus nunc eu eu luctus Integer Tincidunt metus tellus eu massa vitae eros eu lectus porttitor",
+      "price": 14.5,
+      "title": "84.7GHz CPU - Refurbished"
     }, {
-      title: '2.2Ghz CPU - Refurbished',
-      price: 79.12,
-      desc: ''
+      "description": "ligula ligula Sed sodales ac nulla est leo amet hendrerit velit nunc sunt non varius erat faucibus consectetuer cursus tortor placerat tellus Neque et",
+      "price": 8.55,
+      "title": "10.1GHz CPU - Refurbished"
     }, {
-      title: '1.1Ghz CPU - Refurbished',
-      price: 56,
-      desc: ''
+      "description": "dui arcu arcu ligula Sed vel Odio sed phasellus voluptatem justo sed Integer massa hac gravida turpis turpis sed eu est leo vitae eros",
+      "price": 61.05,
+      "title": "131.1GHz CPU"
     }, {
-      title: '1.0Ghz CPU',
-      price: 150.03,
-      desc: ''
+      "description": "aenean lacus lacus pede vitae sed dictumst molestie in arcu consequat dui nullam vestibulum amet est ligula accumsan elementum phasellus",
+      "price": 188.23,
+      "title": "264.7GHz CPU - Refurbished"
     }, {
-      title: '3.21Ghz CPU',
-      price: 893.23,
-      desc: ''
+      "description": "molestie porttitor metus erat metus laoreet integer hac dolor pede nulla luctus amet quis",
+      "price": 37.16,
+      "title": "110.7GHz CPU - Refurbished"
     }, {
-      title: '3.33Ghz CPU',
-      price: 999.99,
-      desc: ''
+      "description": "vivamus elit lacus voluptatem sunt ligula ligula nulla non id erat sed est pede ac Sed lorem metus pretium elit elit hendrerit lacus id vel orci pede aliquam voluptatem est ridiculus vivamus dolor integer tortor",
+      "price": 22.16,
+      "title": "202.3GHz CPU - Refurbished"
     }];
 
   }
